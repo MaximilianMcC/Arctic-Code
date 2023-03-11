f88156c411b766adb7c708a2c21ef6c0b8cb68b1
Max <max@maximilian.co.nz>
2023-03-11 20:12:48 +1300

    Generated data for question 1

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Imports
-const express = require('express');
+const express = require("express");
+const { request } = require("http");
 const path = require("path");
 
 
@@ -13,6 +14,15 @@ app.use(express.static(path.join(__dirname, "public")));
 
 
 
+// Generate data
+app.get("/generate-data", (request, response) => {
+
+    // Get the query parameters to figure out what question they need data for
+    const questionNumber = request.query.question;
+
+    // Generate, then return the data
+    response.send(generateData(questionNumber));
+});
 
 
 
@@ -24,3 +34,71 @@ app.listen(port, () => {
     console.log("Access via localhost: http://127.0.0.1:" + port + "\n");
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Generate data for the questions
+function generateData(questionNumber) {
+    console.log(questionNumber);
+    
+    let response;
+
+    // Check for what question the data is being generated for
+    switch (questionNumber) {
+
+        // Question 1 data
+        case "1":
+
+            // Generate 5 sets of data
+            let data = "";
+            for (let i = 0; i < 5; i++) {
+
+                // Keep generating data, until the data is in a range around about 120
+                let currentTotalSum = 0;
+                while (currentTotalSum <= randomNumber(95, 265)) {
+                    
+                    // Add a random number, and update the sum
+                    const currentValue = randomNumber(1, 10);
+                    data += currentValue + "\n";
+                    currentTotalSum += currentValue;
+                }
+
+                data += "\n"
+            }
+
+            // Return the data
+            response = data
+            break;
+    
+        default:
+            response = 500;
+            break;
+    }
+
+    // Return the generated data, or error code.
+    return response;
+}
+
+
+
+
+
+
+
+
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
